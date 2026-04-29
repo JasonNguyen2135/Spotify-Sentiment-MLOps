dfa586d3ced630d6d39ae9438e7b7b3135e032b7
nguyentrinh2135 <caocaonguyentrinh210305@gmail.com>
2026-04-29 08:29:02 +0700

    fix: remove invalid sqlalchemy import create_all

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from sqlalchemy import create_all, Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from jose import JWTError, jwt
